9966b9a3af45d66f146e0628593c92c1c803c90b
ariel1985 <arielrotem85@gmail.com>
2023-09-06 18:34:59 +0300

    prepare to fix saving the pages content

--- builder/models.py
+++ builder/models.py
@@ -9,7 +9,21 @@ class Pages(models.Model):
     # js = models.TextField() ???
     preview_link = models.TextField()
 
+# TODO: Save grapeJS in djnago...
+# https://chat.openai.com/share/33722e75-cc0e-4c68-a465-d73f111bb550
+
+
+# --- GrapeJS Blocks HTML ---
 # https://chat.openai.com/share/496e5ff5-34c3-4475-96e9-843ff3286e86
+
+from django.db import models
+
+class MediaObject(models.Model):
+    url = models.URLField()
+    alt_text = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.url
 
 class BlockCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -23,18 +37,7 @@ class GrapesJSBlock(models.Model):
     attributes = models.JSONField()
     content = models.TextField()
     category = models.ForeignKey(BlockCategory, on_delete=models.CASCADE)
+    media = models.ForeignKey(MediaObject, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.label
-
-class MediaObject(models.Model):
-    url = models.URLField()
-    alt_text = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.url
-
-class GrapesJSBlock(models.Model):
-    # ... other fields ...
-
-    media = models.ForeignKey(MediaObject, null=True, blank=True, on_delete=models.SET_NULL)
